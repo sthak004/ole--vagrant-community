288c2e7c5f2cd788c2da52f681ebe0aa8cd706ea
pranesh-s <pran1994@gmail.com>
2016-08-09 15:26:16 -0400

    run vagrant on startup script in correct directory (#24)

--- windows/start_vagrant_on_boot.bat
+++ windows/start_vagrant_on_boot.bat
@@ -1,5 +1,6 @@
 ECHO OFF
 start cmd /c copy "C:\Users\%USERNAME%\ole--vagrant-community\windows\start_vagrant_on_boot.bat" "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+cd "C:\Users\%USERNAME%\ole--vagrant-community\windows"
 cd /d %~dp0
 for /f "tokens=2* delims= " %%F IN ('vagrant status ^| find /I "default"') DO (SET "STATE=%%F%%G")
 
